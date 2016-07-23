54ce66ec15828e006beed453641bc9001e8723cc
Umair Ansari <umairda@gmail.com>
2016-07-23 07:57:56 -0700

    fixed templating

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require('./models/connection');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -63,6 +63,7 @@ if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
+	  status: err.status || 500,
       message: err.message,
       error: err
     });
